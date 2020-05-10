01b82bb0c341a10a700783d988cddf0f91d02e81
Trung DUONG VIET <dg.viettrung@gmail.com>
2020-05-10 17:58:44 +0700

    update api /goodbye
    using default service-discovery

--- hello-service/src/main/java/com/example/demo/HelloServiceConfiguration.java
+++ hello-service/src/main/java/com/example/demo/HelloServiceConfiguration.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
@@ -12,12 +10,6 @@ import com.netflix.loadbalancer.RandomRule;
 @Configuration
 @EnableDiscoveryClient
 public class HelloServiceConfiguration {
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	
 	@Bean
 	public IRule iRule() {
--- hello-service/src/main/java/com/example/demo/controller/HelloController.java
+++ hello-service/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,26 +1,41 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
 @RestController
 public class HelloController {
 
 	@Autowired
-	private RestTemplate restTemplate;
+	private EurekaClient client;
+	
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
 	
 	@GetMapping
 	public String hello(@RequestHeader("x-location") String location) {
 		
 		return "Hello from " + location + "!";
 	}
+
 	
 	@GetMapping("/goodbye")
-	public String callGoodbyeService() {
+	public String callGoodbye() {
 		
-		return restTemplate.getForEntity("http://goodbye-service", String.class).getBody();
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = client.getNextServerFromEureka("goodbye-service", false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		ResponseEntity<String> response =
+				restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+		return response.getBody();
 	}
 }
