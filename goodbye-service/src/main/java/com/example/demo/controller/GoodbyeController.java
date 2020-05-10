b82c37a27d05c3c43f294543e2631e81f1358c16
Trung DUONG VIET <dg.viettrung@gmail.com>
2020-05-10 16:46:31 +0700

    add load balacing using @LoadBalanced annotation

--- goodbye-service/src/main/java/com/example/demo/controller/GoodbyeController.java
+++ goodbye-service/src/main/java/com/example/demo/controller/GoodbyeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GoodbyeController {
 
+	@Value("${server.port}")
+	private String port;
+	
 	@GetMapping
 	public String goodBye() {
-		return "Goodbye!";
+		return String.format("The current time is: %s.\n Goodbye! (from goodbye-service with port: %s)", new Date(), port);
 	}
 }
--- hello-service/src/main/java/com/example/demo/controller/HelloController.java
+++ hello-service/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@GetMapping
 	public String hello(@RequestHeader("x-location") String location) {
+		
 		return "Hello from " + location + "!";
+	}
+	
+	@GetMapping("/goodbye")
+	public String callGoodbyeService() {
+		
+		return restTemplate.getForEntity("http://goodbye-service", String.class).getBody();
 	}
 }
